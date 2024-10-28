3ee7c3e53087ffa807b58898bfff3b24447beee5
anhvu <nguyentrananhvu04@gmail.com>
2024-10-28 17:22:44 +0700

    __

--- kcs-frontend/src/components/Footer/Footer1.jsx
+++ kcs-frontend/src/components/Footer/Footer1.jsx
@@ -7,11 +7,11 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
-  // Define the inline styles as objects
-  
-
   const iconStyle = {
     margin: "0 15px",
+    cursor: "pointer",
+    transition: "color 0.3s ease",
+    color: "#fff",
   };
 
   const socialIconsStyle = {
@@ -27,49 +27,49 @@ function Footer() {
     fontFamily: "JetBrains Mono",
   };
 
+  const footerStyle = {
+    backgroundColor: "#212121",
+    padding: "20px",
+    color: "#fff",
+    width: "100%",
+    position: "fixed",
+    bottom: 0,
+    left: 0,
+    zIndex: 1000,
+  };
+
   return (
-    <>
-      
+    <footer style={footerStyle}>
+      <div className="row">
         <div
-          className="row mt-5"
-          style={{
-            width:'100%',
-            backgroundColor: "#212121",
-            padding: "20px",
-            position: "relative",
-            color: "#fff",
-          }}
+          className="d-flex justify-content-around align-items-center"
+          style={fontStyle}
         >
-          <div
-            className="d-flex justify-content-around align-items-center"
-            style={fontStyle}
-          >
-            <div style={{ fontFamily: "JetBrains Mono", fontSize: "15px" }}>
-              &copy; 2024 Bảo lưu mọi quyền, Koiday&reg;
-              <br />
-              Chính sách quyền riêng tư | Điều khoản
-            </div>
-            <div className="social-icons" style={socialIconsStyle}>
-              <a>
-                <i style={iconStyle}>
-                  <FontAwesomeIcon icon={faFacebook} />
-                </i>
-              </a>
-              <a>
-                <i style={iconStyle}>
-                  <FontAwesomeIcon icon={faTelegram} />
-                </i>
-              </a>
-              <a>
-                <i style={iconStyle}>
-                  <FontAwesomeIcon icon={faPinterest} />
-                </i>
-              </a>
-            </div>
+          <div style={{ fontFamily: "JetBrains Mono", fontSize: "15px" }}>
+            &copy; 2024 Bảo lưu mọi quyền, Koiday&reg;
+            <br />
+            Chính sách quyền riêng tư | Điều khoản
+          </div>
+          <div className="social-icons" style={socialIconsStyle}>
+            <a>
+              <i style={iconStyle}>
+                <FontAwesomeIcon icon={faFacebook} />
+              </i>
+            </a>
+            <a>
+              <i style={iconStyle}>
+                <FontAwesomeIcon icon={faTelegram} />
+              </i>
+            </a>
+            <a>
+              <i style={iconStyle}>
+                <FontAwesomeIcon icon={faPinterest} />
+              </i>
+            </a>
           </div>
         </div>
-      
-    </>
+      </div>
+    </footer>
   );
 }
 
